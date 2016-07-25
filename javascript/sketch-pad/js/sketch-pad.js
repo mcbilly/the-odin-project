17f5aef1ea4b731c525103b2df9b9b3d94dc222a
McBilly <mcbillywilford@gmail.com>
2016-07-24 22:59:12 -0400

    added new sketchpad button

--- javascript/sketch-pad/js/sketch-pad.js
+++ javascript/sketch-pad/js/sketch-pad.js
@@ -4,6 +4,22 @@ var gridNumInt = 16; //how many initial grids of blocks starting with 16x16
 
 $(document).ready(function(){
 
+	startSketchPad(gridNumInt);
+
+	$('button').click(function(){
+		var newGridNum = prompt("Enter how many grids for your new sketchpad:");
+		clearSketchPad();
+		startSketchPad(newGridNum);
+		drawSketch('.block');
+	});
+
+	drawSketch('.block');
+
+});
+
+
+function startSketchPad(gridNumInt){ //initialize sketchpad with number of grids
+
 	for(var x=0; x < gridNumInt; x++){
 
 		for(var y=0; y < gridNumInt; y++){
@@ -15,11 +31,14 @@ $(document).ready(function(){
 		$('#sketchpad').append("<div style='clear:both'></div>")
 	}
 
-	$('.block').hover(
-		function(){
-			$(this).addClass('onHover');
-		}
-	);
+};
 
-});
+function clearSketchPad(){ //clear sketchpad
+	$('#sketchpad').empty();
+}
 
+function drawSketch(block){ //start drawing
+	$(block).hover(function(){
+		$(this).addClass('onHover');
+	});
+}
