f514d0b6be29bc8700dcd65239d3ece71d908b4c
McBilly <mcbillywilford@gmail.com>
2016-08-09 00:52:22 -0400

    added paperclip image attachment feature

--- rails/blogger/app/models/article.rb
+++ rails/blogger/app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ApplicationRecord
 	has_many :taggings
 	has_many :tags, through: :taggings
 
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>"}
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 	def tag_list
 		self.tags.collect do |tag|
 			tag.name
@@ -14,4 +17,5 @@ class Article < ApplicationRecord
 		new_or_found_tags = tag_name.collect { |name| Tag.find_or_create_by(name: name)}
 		self.tags = new_or_found_tags
 	end
+
 end
